c4bc1b5ac20f1456a95b2a736a2cb268d983fe4a
Alex CORBEL <alexcorbel97@gmail.com>
2019-10-08 23:54:02 +0200

    ajout graph2

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,35 +1,30 @@
 <template>
   <div class="container">
-    <line-chart :chartdata="chartData" :options="options"/>
+    <line-chart/>
   </div>
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs'
+
+import { Line } from 'vue-chartjs'
 
 export default {
-  extends: Bar,
-  data: () => ({
-    chartdata: {
-      labels: ['January', 'February'],
+  extends: Line,
+  mounted () {
+    // Overwriting base render method with actual data.
+    this.renderChart({
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
       datasets: [
         {
-          label: 'Data One',
+          label: 'GitHub Commits',
           backgroundColor: '#f87979',
-          data: [40, 20]
+          data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
         }
       ]
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false
-    }
-  }),
-
-  mounted () {
-    this.renderChart(this.chartdata, this.options)
+    })
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->

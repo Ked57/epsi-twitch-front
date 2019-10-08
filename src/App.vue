ba1eadc254961a911d7b918c5ba51e900866719b
Alex CORBEL <alexcorbel97@gmail.com>
2019-10-08 21:37:29 +0200

    ajout vuejs

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+<body>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <h1 class="navbar-item has-text-white">Twitch Statistique</h1>
+
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item has-text-white">Home</a>
+      </div>
+    </div>
+  </nav>
+  <List></List>
+</body>
+</template>
+
+<script>
+import List from "./components/List.vue";
+
+export default {
+  name: "app",
+  components: {
+    List
+  }
+}; 
+</script>
+
+<style>
+@import "~bulma/css/bulma.css";
+.navbar{
+  background-color: #6441a5;
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="container is-centered app">
+  <br>
+  <h1 class="title is-text-centered ">Liste des jeux</h1>
+  <table class="table is-bordered is-striped is-truncated">
+		<thead>
+			<tr>
+				<th>Nom du jeu</th>
+        <th>Nombre de vues</th>
+        <th>Bonus</th>
+			</tr>
+		</thead>
+		<tbody  v-for="game in games" :key="game.id" >
+      <tr>
+				<td>{{ game.name }}</td>
+				<td>{{ game.username }}</td>
+        <td>wow</td>
+			</tr>	
+		</tbody>
+	</table>
+  </div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      games:[]
+    }
+  },
+  methods: {
+    getGames() {
+      const axios = require('axios');
+      axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        this.games = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+  },
+  created: function (){
+    this.getGames()
+  } 
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
